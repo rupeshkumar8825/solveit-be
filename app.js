9df38e4247fd64b37e874ad521e203eea25a6928
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2022-07-27 02:41:36 +0530

    made some changes these were very painful errors

--- app.js
+++ app.js
@@ -37,12 +37,12 @@ app.use(cookieParser());
 app.get("/", async (req, res)=>{
     // console.log("The cookie that i get from the user is as follows \n");
     let ideas = await ideaModel.find();
-    console.log("The list of ideas that are stored in the database are as follows \n");
-    console.log(ideas);
+    // console.log("The list of ideas that are stored in the database are as follows \n");
+    // console.log(ideas);
    
     let users = await userModel.find();
-    console.log("The list of users are as follows \n");
-    console.log(users);
+    // console.log("The list of users are as follows \n");
+    // console.log(users);
 
     // SO WE WILL ALSO SEND  THIS INFO TO THE FRONTEND ALONG WITH THE OTHER DETAILS FOR THIS PURPOSE 
     try{
@@ -51,16 +51,16 @@ app.get("/", async (req, res)=>{
         console.log("got the get request in the home page\n");
         console.log(req.cookies)
         const token = req.cookies.token;
-        // console.log(token);
-        // console.log("The current user is as follows");
+        console.log(token);
+        console.log("The current user is as follows");
         let curr_user = jwt.verify(token, process.env.SECRET_KEY);
         curr_user = await userModel.findOne({_id : curr_user._id});
         
         
 
         
-        // console.log(curr_user);
-        res.status(200).sendFile({status : 200, message : "ok", curr_user, ideas : ideas, users : users});
+        console.log(curr_user);
+        res.status(200).json({status : 200, message : "ok", curr_user, ideas : ideas, users : users});
     }catch(error){
         console.log("got some error in error section")
         // res.sendFile(`${fileLocation}`);
@@ -236,14 +236,14 @@ app.post("/upload", upload.single("image"), async (req, res) => {
 
 // MAKING AN ENDPOINT TO SEND THE PHOTO REQUEST BY THE FRONTEND FOR THIS PURPOSE 
 app.get("/image", (req, res)=>{
-    console.log("The frontend wants this image and hence i  will be sending to the frontend \n")
-    console.log("The body is as follows \n");
-    console.log(req.query);
+    // console.log("The frontend wants this image and hence i  will be sending to the frontend \n")
+    // console.log("The body is as follows \n");
+    // console.log(req.query);
 
     // NOW ONCE WE GOT THE PARAMS WE HAVE TO SENT BACK THIS IMAGE TO THE FRONTEND THAT WILL BE STORED
     let fileLocation = path.join(__dirname,`./${req.query.path}`);
-    console.log("The location of the file is as follows \n\n");
-    console.log(fileLocation);    
+    // console.log("The location of the file is as follows \n\n");
+    // console.log(fileLocation);    
 
     // res.json({message : "Got and sent the image"})
     res.sendFile(`${fileLocation}`);
