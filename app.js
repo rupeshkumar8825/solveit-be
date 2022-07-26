d9f7010b321bda7a8a85afc4a80d249afdeeb141
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2022-07-26 20:31:23 +0530

    Added the functionality to send the image being asked by frontend using URL params

--- app.js
+++ app.js
@@ -36,17 +36,17 @@ app.use(cookieParser());
 
 app.get("/", async (req, res)=>{
     // console.log("The cookie that i get from the user is as follows \n");
-    // let ideas = await ideaModel.find();
-    // console.log("The list of ideas that are stored in the database are as follows \n");
-    // console.log(ideas);
-    let fileLocation = path.join(__dirname,"./uploads/1658174707661--basic_expectation.png");
-    console.log("The location of the file is as follows \n\n");
-    console.log(fileLocation);
+    let ideas = await ideaModel.find();
+    console.log("The list of ideas that are stored in the database are as follows \n");
+    console.log(ideas);
+    // let fileLocation = path.join(__dirname,"./uploads/1658174707661--basic_expectation.png");
+    // console.log("The location of the file is as follows \n\n");
+    // console.log(fileLocation);
     // WE HAVE ALSO HAVE TO SEARCH THE USER DETAILS BASED ON THE IDEAS USER_ID SO THAT WE CAN SEND ALL THE DETAILS 
     // WE HAVE TO MAKE A SEPARATE OBJECT WHICH WILL STORE ALL THERSE INFORMATIONS WITH THE USER INFORMATION AND THE IDEAS INFORMATIONS AS WELL FOR THIS PURPOSE 
-    // let users = await userModel.find();
-    // console.log("The list of users are as follows \n");
-    // console.log(users);
+    let users = await userModel.find();
+    console.log("The list of users are as follows \n");
+    console.log(users);
 
     // SO WE WILL ALSO SEND  THIS INFO TO THE FRONTEND ALONG WITH THE OTHER DETAILS FOR THIS PURPOSE 
     try{
@@ -67,8 +67,8 @@ app.get("/", async (req, res)=>{
         res.status(200).sendFile({status : 200, message : "ok", curr_user, fileLocation : fileLocation});
     }catch(error){
         console.log("got some error in error section")
-        res.sendFile(`${fileLocation}`);
-        // res.sendFile({status : 401, message : "so not ok", fileLocation : fileLocation});
+        // res.sendFile(`${fileLocation}`);
+        res.json({status : 401, message : "so not ok", ideas : ideas, users: users});
         // res.end();
         return;
     }
@@ -237,6 +237,21 @@ app.post("/upload", upload.single("image"), async (req, res) => {
 
 })
 
+
+// MAKING AN ENDPOINT TO SEND THE PHOTO REQUEST BY THE FRONTEND FOR THIS PURPOSE 
+app.get("/image", (req, res)=>{
+    console.log("The frontend wants this image and hence i  will be sending to the frontend \n")
+    console.log("The body is as follows \n");
+    console.log(req.query);
+
+    // NOW ONCE WE GOT THE PARAMS WE HAVE TO SENT BACK THIS IMAGE TO THE FRONTEND THAT WILL BE STORED
+    let fileLocation = path.join(__dirname,`./${req.query.path}`);
+    console.log("The location of the file is as follows \n\n");
+    console.log(fileLocation);    
+
+    // res.json({message : "Got and sent the image"})
+    res.sendFile(`${fileLocation}`);
+})
 
 
 // app.get("/ideas", )
